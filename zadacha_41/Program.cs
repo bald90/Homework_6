6d7388b4a38775a44844ce156807bb2a1e9f64e5
Andrey <6198760@gmail.com>
2022-12-18 17:44:53 +0300

    solve zadach 41v2

--- zadacha_41/Program.cs
+++ zadacha_41/Program.cs
@@ -2,29 +2,85 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 4
 
+///Цель:
+///1. ввести размер массива
+///2. заполнить его рандомными числами
+///3. определить больше 0
+///4. вывести на экран
+
 Console.Clear();
 
+//Вариант № 1
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);  // подсмотрел решение в сети.      
+int count = 0;
 
-int DataEntry(string str)                   // 1.1. Метод ввода данных
+for (int i = 0; i < arr.Length; i++)
 {
-    Console.WriteLine(str);
-    int number = int.Parse(Console.ReadLine());
-    return number;
+    if (arr[i] > 0)
+    {
+        count++;
+    }
 }
 
-int number1 = DataEntry("Введите число 1: ");    // 1.0 Запросить число
-int number2 = DataEntry("Введите число 2: ");    // 1.0 Запросить число
-int number3 = DataEntry("Введите число 3: ");    // 1.0 Запросить число
-int number4 = DataEntry("Введите число 4: ");    // 1.0 Запросить число
-int number5 = DataEntry("Введите число 5: ");    // 1.0 Запросить число
+Console.WriteLine("-> " + count);
 
 
-int count = 0;
-if (number1 > 0) count = 1;
-if (number2 > 0) count++;
-if (number3 > 0) count++;
-if (number4 > 0) count++;
-if (number5 > 0) count++;
+// Вариант № 2. Предположил что пользователь может ввести потенциальный размер массива...
 
-Console.WriteLine("-> " + count);        // Вывести колличество
+// int DataEntry(string str)                       // Метод ввода данных
+// {
+//     Console.WriteLine(str);
+//     int SIZE = int.Parse(Console.ReadLine());
+//     return SIZE;
+// }
+
+// int SIZE = DataEntry("введите число = ");     // просим ввести число (потенциально разер массива)
+
+// const int LEFT_RANGE = -9;
+// const int RIGHT_RANGE = 9;
+// int[] array = new int[SIZE];  //создали массив
+
+// int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+// {
+//     Random rand = new Random();             //создали объект класса рандом
+//     int[] arr = new int[size];              // объявили массив
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = rand.Next(leftRange, rightRange + 1);
+//         // arr[0] = 1;
+//         // arr[1] = -7;
+//         // arr[2] = 567;
+//         // arr[3] = 89;
+//         // arr[4] = 223;
+//     }
+//     return arr;
+// }
+// array = FillArrayWithRandomNumbers(SIZE, LEFT_RANGE, RIGHT_RANGE);
+// System.Console.WriteLine('[' + string.Join(", ", array) + ']');
+
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] > 0)
+//     {
+//         count += 1;
+//     }
+// }
+// System.Console.WriteLine("-> " + count);
+
+
+
+//Вариант № 3 из сети...
+// Console.Write("Введите элементы(через пробел): ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);   
+// int count = arr.Count(x => x > 0);                                   //не понял что такое "х" , чем оно определяется и что означает запись x => x>0
+// Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+
+
+
+
+
+
 
